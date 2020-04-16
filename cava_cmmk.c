cbf451c2b13f81931daa29622cd53b10d37c5604
Bonnee <bonora.matteo@gmail.com>
2020-04-16 21:28:06 +0200

    Add config

--- cava_cmmk.c
+++ cava_cmmk.c
@@ -7,6 +7,7 @@
 
 #include "keeb.h"
 
+#define CAVA_CMD "cava -p "
 #define BAR_N 22
 
 int g_stop = 0;
@@ -18,18 +19,30 @@ static void interrupted(int sig) {
 }
 
 int main(int argc, char *argv[]) {
-	signal(SIGINT, interrupted);
+	if (argc != 2) {
+		printf("usage %s <cava_config>\n", argv[0]);
 
-	if (keeb_init() != 0) {
-		fprintf(stderr, "Unable to load keyboard\n");
 		exit(EXIT_FAILURE);
 	}
 
+	signal(SIGINT, interrupted);
+
 	// Run cava
-	FILE *stream = popen("cava", "r");
+	char cava_cmd[255];
+	sprintf(cava_cmd, CAVA_CMD "%s", argv[1]);
+
+	printf("cmd: %s", cava_cmd);
+
+	FILE *stream = popen(cava_cmd, "r");
 
 	if (stream == NULL) {
 		fprintf(stderr, "Unable to open stream\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Init keeb
+	if (keeb_init() != 0) {
+		fprintf(stderr, "Unable to load keyboard\n");
 		exit(EXIT_FAILURE);
 	}
 
